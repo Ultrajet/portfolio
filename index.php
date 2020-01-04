ae49757718aeb06b88a8f52d7d05fc78be429fa8
Ultrajet <51157704+Ultrajet@users.noreply.github.com>
2020-01-04 20:35:51 +0100

    ajout contenu + thème

--- index.php
+++ index.php
@@ -8,15 +8,16 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <!-- link rel="stylesheet" href="css/style.css" -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
     <title>Document</title>
 </head>
 
 <body>
 
-    <div class="container col-6 border">
+    <div class="container col-md-6 border">
         <header class="d-flex justify-content-between flex-wrap">
-            <img src="./assets/me.jpg" alt="Quentin Lagadou" class="img-thumbnail rounded-circle">
-            <div class="text-center align-self-center">
+            <img src="./assets/me.jpg" alt="Quentin Lagadou" class="mx-auto img-thumbnail rounded-circle">
+            <div class="mx-auto text-center align-self-center">
                 <h1>Quentin Lagadou</h1>
                 <h2>Développeur Web Junior</h2>
                 <h3>PHP/JS/SQL - Symfony 4</h3>
@@ -24,6 +25,110 @@
             </div>
         </header>
         <hr>
+        <main>
+            <div id="projets">
+                <h3>projets open-source</h3>
+                <section>
+                    <h4 class="text-center">logo - Walk & Meet</h3>
+                        <p class="text-justify">Companion app pour les jeux à réalité augmentée (type
+                            Pokémon Go). Les utilisateurs peuvent se géolocaliser afin
+                            de rencontrer des personnes dans leurs environs et jouer
+                            avec eux. Ils disposent d’une messagerie instantanée afin de
+                            communiquer. Projet de soutenance pour le titre
+                            professionnel Développeur web et web mobile.</p>
+                        <p class="text-center">logo - <a
+                                href="https://github.com/Ultrajet/projetgo">https://github.com/Ultrajet/projetgo</a></p>
+                        <button type="button" class="d-block mx-auto btn btn-info">Screenshots</button>
+                        <p><em>Equipe : deux développeurs (front + full-stack), un web designer et un chef de
+                                projet<br>
+                                #socialnetwork #symfony #php #mysql #javascript #jquery #ajax #bootstrap #leaflet
+                                #mapbox #openstreetmap #mobilefirst</em></p>
+                </section>
+                <hr>
+                <section>
+                    <h4 class="text-center">logo - Geek’ation</h3>
+                        <p class="text-justify">Application web pour une boutique de location de matériel
+                            gaming (console, jeux et accessoires). Le site permet une
+                            gestion des stocks, commandes et utilisateurs pour les
+                            administrateurs. Les utilisateurs peuvent louer pour autant
+                            de temps qu’ils veulent, à partir de 24 heures. Projet de
+                            soutenance pour Webforce3.</p>
+                        <p class="text-center">logo - <a
+                                href="https://github.com/Ultrajet/Geekation">https://github.com/Ultrajet/Geekation</a>
+                        </p>
+                        <button type="button" class="d-block mx-auto btn btn-info">Screenshots</button>
+                        <p><em>Equipe : quatre développeurs, deux front et deux back<br>
+                                #ecommerce #symfony #php #mysql #javascript #jquery #ajax #bootstrap
+                                #giantbomb #responsivedesign</em></p>
+                </section>
+                <hr>
+                <section>
+                    <h4 class="text-center">logo - We’R Généalogie (en cours)</h3>
+                        <p class="text-justify">Site web servant d’outil pour générer des arbres
+                            généalogiques dynamiques. L’affichage gère pour le moment
+                            deux générations, parmi lesquelles le visiteur peut naviguer
+                            en un clic. L’utilisateur pourra renseigner sa propre famille en
+                            base de données, puis obtenir un rendu visuel propre,
+                            entièrement responsive et exportable. Ce site, développé en
+                            duo, sert d’expérimentation, entre autre pour savoir jusqu’où
+                            il est possible de coder en PHP procédural avant de migrer
+                            sur un framework MVC type Symfony.</p>
+                        <p class="text-center">logo - <a
+                                href="https://github.com/bldlr/Genealogie">https://github.com/bldlr/Genealogie</a></p>
+                        <button type="button" class="d-block mx-auto btn btn-info">Screenshots</button>
+                        <p><em>Equipe : deux développeurs full-stack<br>
+                                #plaisir #php #mysql #javascript #jquery #ajax #bootstrap #symfony
+                                #responsivedesign</em></p>
+                </section>
+            </div>
+            <hr>
+            <div id="apropos">
+                <section>
+                    <h4>à propos de moi</h4>
+                    <p class="text-danger text-justify">Je suis diplômé depuis novembre 2019 d'un titre professionnel
+                        niveau III
+                        Développeur web / web mobile, qui fait suite à une formation intensive de
+                        600 heures chez Webforce3.</p>
+                    <button type="button" class="d-block mx-auto btn btn-info">En savoir plus</button>
+                    <p class="text-justify">Mon aventure de développeur commence au début de mon adolescence, en
+                        faisant mes armes sur feu Le Site du Zéro où j ai approché le HTML/CSS
+                        d époque, ainsi que le PHP procédural. J ai immédiatement accroché au
+                        dialogue avec la machine informatique et j ai découvert un terrain de jeu
+                        infini dont les seules limites étaient mon imagination.<br>
+                        Plusieurs mods de jeux et expérimentations de musique électronique plus
+                        tard, j ai rejoint Webforce3 en 2019 pour me former professionnellement au
+                        métier de Développeur Web. Ici j ai rencontré des professeurs qui m ont
+                        permis d asseoir mes connaissances, ainsi que de partager ma passion
+                        avec des développeurs en herbe dont certains sont devenus par la suite des
+                        amis. Depuis, je monte en expérience en poursuivant des projets en équipe,
+                        où je peux mettre au service du collectif mon esprit hybride
+                        technologique-artistique.</p>
+                    <p class="text-center">Mon CV : logo</p>
+                </section>
+            </div>
+            <hr>
+            <div id="competences">
+                <h4>mes compétences techniques</h4>
+                <p>
+                    langages :<br>
+                    HTML, CSS, Javascript + jQuery, PHP, MySQL
+                </p>
+                <p class="text-right">
+                    librairies/frameworks :<br>
+                    Bootstrap, Wordpress, React, Symfony
+                </p>
+                <p>
+                    outils :<br>
+                    Visual Studio Code, XAMPP, Git + GitHub, Trello, Slack,
+                    Figma
+                </p>
+                <p class="text-right">
+                    ce que j'apprends / ce que j'aimerais apprendre bientôt :<br>
+                    NodeJS, Java (J2E), Python, Docker, Kubernetes
+                </p>
+            </div>
+        </main>
+
     </div>
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
