90107b172ec04ef2492bb549afd6875706475a33
Ultrajet <51157704+Ultrajet@users.noreply.github.com>
2020-01-07 19:56:14 +0100

    more and more

--- index.php
+++ index.php
@@ -46,8 +46,9 @@
                 <h1 class="text-info mb-0">Quentin Lagadou</h1>
                 <h2 class="mb-0" style="font-size: 2rem">Développeur Web Junior</h2>
                 <h3 style="font-size: 1.5rem">PHP/JS/SQL - Symfony 4</h3>
-                <span><img src="assets/github.png" height="25" class="pr-2"><img src="assets/linkedin.png"
-                        height="25"></span>
+                <span><a href="https://github.com/Ultrajet"><img src="assets/github.png" height="25" class="pr-2"></a><a
+                        href="https://www.linkedin.com/in/quentin-lagadou/"><img src="assets/linkedin.png"
+                            height="25"></a></span>
             </div>
         </header>
         <hr class="my-4">
@@ -55,37 +56,134 @@
             <div id="projets">
                 <h3 class="mb-4">projets open-source</h3>
                 <section>
+
                     <h4 class="text-center"><img src="assets/walk&meet.png" height="30"> <em>Walk &
                             Meet</em></h4>
                     <p class="text-justify description">Companion app pour les jeux à réalité augmentée (type
                         Pokémon Go). Les utilisateurs peuvent se géolocaliser afin
                         de rencontrer des personnes dans leurs environs et jouer
-                        avec eux. Ils disposent d’une messagerie instantanée afin de
+                        avec eux. Ils disposent d'une messagerie instantanée afin de
                         communiquer. Projet de soutenance pour le titre
                         professionnel Développeur web et web mobile.</p>
-                    <p class="text-center mb-0"><img src="assets/git_repo.png" height="30" class="pr-1"> <a
-                            href="https://github.com/Ultrajet/projetgo">https://github.com/Ultrajet/projetgo</a></p>
-                    <button type="button" class="d-block mx-auto btn btn-info mb-3">Screenshots</button>
-                    <p class="text-justify"><em>Equipe : deux développeurs (front + full-stack), un web designer et un
+                    <a href="https://github.com/Ultrajet/projetgo">
+                        <div class="d-flex justify-content-center align-items-center flex-wrap">
+                            <img src="assets/git_repo.png" height="30" class="pr-1">
+                            https://github.com/Ultrajet/projetgo
+                        </div>
+                    </a>
+
+                    <button type="button" class="d-block mx-auto btn btn-info mb-4" data-toggle="modal"
+                        data-target="#screensWalk">
+                        Screenshots
+                    </button>
+
+                    <!-- Modal -->
+                    <div class="modal fade" id="screensWalk" tabindex="-1" role="dialog" aria-hidden="true">
+                        <div class="modal-dialog modal-dialog-centered modal-sm" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title">Screenshots</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body">
+                                    <div id="carouselWalk" class="carousel slide" data-ride="carousel">
+                                        <div class="carousel-inner">
+                                            <div class="carousel-item active">
+                                                <img class="d-block w-100" src="assets/w&m1.png" alt="First slide">
+                                            </div>
+                                            <div class="carousel-item">
+                                                <img class="d-block w-100" src="assets/w&m2.png" alt="Second slide">
+                                            </div>
+                                            <div class="carousel-item">
+                                                <img class="d-block w-100" src="assets/w&m3.png" alt="Third slide">
+                                            </div>
+                                        </div>
+                                        <a class="carousel-control-prev" href="#carouselWalk" role="button"
+                                            data-slide="prev">
+                                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                            <span class="sr-only">Previous</span>
+                                        </a>
+                                        <a class="carousel-control-next" href="#carouselWalk" role="button"
+                                            data-slide="next">
+                                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                            <span class="sr-only">Next</span>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <p class="text-justify"><em>Équipe : deux développeurs (front + full-stack), un web designer et un
                             chef de
                             projet<br>
                             #socialnetwork #symfony #php #mysql #javascript #jquery #ajax #bootstrap #leaflet
                             #mapbox #openstreetmap #mobilefirst</em></p>
+
                 </section>
                 <hr>
                 <section>
-                    <h4 class="text-center"><img src="assets/geekation.png" height="40"> <em>Geek’ation</em></h4>
+                    <h4 class="text-center"><img src="assets/geekation.png" height="40"> <em>Geek'ation</em></h4>
                     <p class="text-justify description">Application web pour une boutique de location de matériel
                         gaming (console, jeux et accessoires). Le site permet une
                         gestion des stocks, commandes et utilisateurs pour les
                         administrateurs. Les utilisateurs peuvent louer pour autant
                         de temps qu’ils veulent, à partir de 24 heures. Projet de
                         soutenance pour Webforce3.</p>
-                    <p class="text-center mb-0"><img src="assets/git_repo.png" height="30" class="pr-1"> <a
-                            href="https://github.com/Ultrajet/Geekation">https://github.com/Ultrajet/Geekation</a>
-                    </p>
-                    <button type="button" class="d-block mx-auto btn btn-info mb-3">Screenshots</button>
-                    <p class="text-justify"><em>Equipe : quatre développeurs, deux front et deux back<br>
+                    <a href="https://github.com/Ultrajet/Geekation">
+                        <div class="d-flex justify-content-center align-items-center flex-wrap">
+                            <img src="assets/git_repo.png" height="30" class="pr-1">
+                            https://github.com/Ultrajet/Geekation
+                        </div>
+                    </a>
+
+                    <button type="button" class="d-block mx-auto btn btn-info mb-4" data-toggle="modal"
+                        data-target="#screensGeek">
+                        Screenshots
+                    </button>
+
+                    <!-- Modal -->
+                    <div class="modal fade" id="screensGeek" tabindex="-1" role="dialog" aria-hidden="true">
+                        <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title">Screenshots</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body">
+                                    <div id="carouselGeek" class="carousel slide" data-ride="carousel">
+                                        <div class="carousel-inner">
+                                            <div class="carousel-item active">
+                                                <img class="d-block w-100" src="assets/geek1.png" alt="First slide">
+                                            </div>
+                                            <div class="carousel-item">
+                                                <img class="d-block w-100" src="assets/geek2.png" alt="Second slide">
+                                            </div>
+                                            <div class="carousel-item">
+                                                <img class="d-block w-100" src="assets/geek3.png" alt="Third slide">
+                                            </div>
+                                        </div>
+                                        <a class="carousel-control-prev" href="#carouselGeek" role="button"
+                                            data-slide="prev">
+                                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                            <span class="sr-only">Previous</span>
+                                        </a>
+                                        <a class="carousel-control-next" href="#carouselGeek" role="button"
+                                            data-slide="next">
+                                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                            <span class="sr-only">Next</span>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <p class="text-justify"><em>Équipe : quatre développeurs, deux front et deux back<br>
                             #ecommerce #symfony #php #mysql #javascript #jquery #ajax #bootstrap
                             #giantbomb #responsivedesign</em></p>
                 </section>
@@ -93,19 +191,23 @@
                 <section>
                     <h4 class="text-center"><img src="assets/genealogie.png" height="40"> <em>We'R Généalogie <small>(en
                                 cours)</small></em></h4>
-                    <p class="text-justify description">Site web servant d’outil pour générer des arbres
-                        généalogiques dynamiques. L’affichage gère pour le moment
+                    <p class="text-justify description">Site web servant d'outil pour générer des arbres
+                        généalogiques dynamiques. L'affichage gère pour le moment
                         deux générations, parmi lesquelles le visiteur peut naviguer
-                        en un clic. L’utilisateur pourra renseigner sa propre famille en
+                        en un clic. L'utilisateur pourra renseigner sa propre famille en
                         base de données, puis obtenir un rendu visuel propre,
                         entièrement responsive et exportable. Ce site, développé en
-                        duo, sert d’expérimentation, entre autre pour savoir jusqu’où
+                        duo, sert d'expérimentation, entre autre pour savoir jusqu'où
                         il est possible de coder en PHP procédural avant de migrer
                         sur un framework MVC type Symfony.</p>
-                    <p class="text-center mb-0"><img src="assets/git_repo.png" height="30" class="pr-1"> <a
-                            href="https://github.com/bldlr/Genealogie">https://github.com/bldlr/Genealogie</a></p>
-                    <button type="button" class="d-block mx-auto btn btn-info mb-3">Screenshots</button>
-                    <p class="text-justify"><em>Equipe : deux développeurs full-stack<br>
+                    <a href="https://github.com/bldlr/Genealogie">
+                        <div class="d-flex justify-content-center align-items-center flex-wrap mb-4">
+                            <img src="assets/git_repo.png" height="30" class="pr-1">
+                            https://github.com/bldlr/Genealogie
+                        </div>
+                    </a>
+
+                    <p class="text-justify"><em>Équipe : deux développeurs full-stack<br>
                             #plaisir #php #mysql #javascript #jquery #ajax #bootstrap #symfony
                             #responsivedesign</em></p>
                 </section>
@@ -118,21 +220,30 @@
                         niveau III
                         Développeur web / web mobile, qui fait suite à une formation intensive de
                         600 heures chez Webforce3.</p>
-                    <button type="button" class="d-block mx-auto btn btn-info">En savoir plus</button>
-                    <p class="text-justify">Mon aventure de développeur commence au début de mon adolescence, en
-                        faisant mes armes sur feu Le Site du Zéro où j ai approché le HTML/CSS
-                        d époque, ainsi que le PHP procédural. J ai immédiatement accroché au
-                        dialogue avec la machine informatique et j ai découvert un terrain de jeu
-                        infini dont les seules limites étaient mon imagination.<br>
-                        Plusieurs mods de jeux et expérimentations de musique électronique plus
-                        tard, j ai rejoint Webforce3 en 2019 pour me former professionnellement au
-                        métier de Développeur Web. Ici j ai rencontré des professeurs qui m ont
-                        permis d asseoir mes connaissances, ainsi que de partager ma passion
-                        avec des développeurs en herbe dont certains sont devenus par la suite des
-                        amis. Depuis, je monte en expérience en poursuivant des projets en équipe,
-                        où je peux mettre au service du collectif mon esprit hybride
-                        technologique-artistique.</p>
-                    <p class="text-center">Mon CV : <img src="assets/pdf.png" height="40"></p>
+
+                    <button type="button" class="d-block mx-auto btn btn-info mb-2" data-toggle="modal"
+                        data-target="#modalApropos">En savoir plus</button>
+
+                    <!-- Modal -->
+                    <div class="modal fade" id="modalApropos" tabindex="-1" role="dialog" aria-hidden="true">
+                        <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title">un peu plus sur moi</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body">
+                                    <p class="text-justify">Mon aventure de développeur commence au début de mon adolescence, en faisant mes armes sur feu Le Site du Zéro où j'ai approché le HTML/CSS d'époque, ainsi que le PHP procédural. J'ai immédiatement accroché au dialogue avec la machine informatique et j'ai découvert un terrain de jeu infini dont les seules limites sont mon imagination.<br>
+                                    <br>
+                                    Plusieurs mods de jeux et expérimentations de musique électronique plus tard, j'ai rejoint Webforce3 en 2019 pour me former professionnellement au métier de Développeur Web. Ici j'ai rencontré des professeurs qui m'ont permis d'asseoir mes connaissances, ainsi que de partager ma passion avec des développeurs en herbe dont certains sont devenus par la suite des amis. Depuis, je monte en expérience en poursuivant des projets en équipe, où je peux mettre au service du collectif mon esprit hybride technologique-artistique.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <p class="text-center">Mon CV : <a href="#"><img src="assets/pdf.png" height="40"></a></p>
                 </section>
             </div>
             <hr class="my-4">
