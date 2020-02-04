6db33fc253bfd95ea3c8aefb13a84cee409591ee
Ultrajet <51157704+Ultrajet@users.noreply.github.com>
2020-02-05 00:17:55 +0100

    fix navbar.js

--- js/navbar.js
+++ js/navbar.js
@@ -2,7 +2,7 @@ $(document).ready(() => {
 
     var divId;
 
-    $('.nav-link, .navbar-brand, nav button').click(function () {
+    $('.nav-link, .navbar-brand, nav button.btn-success').click(function () {
         divId = $(this).attr('href');
         $('html, body').animate({
             scrollTop: $(divId).offset().top - 80
